5a46591ee31d9d29f0246a74a08db4105d494928
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-29 21:59:07 +0700

    binarysearch kelar

--- binary.js
+++ binary.js
@@ -27,8 +27,21 @@ var test_array_genap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 var test_array_ganjil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 function binary_search (search, array) {
-  // Your code here
+  var lo=0;
+  var hi=array.length-1;
 
+
+  while (lo<=hi){
+    var mid= Math.floor((hi+lo)/2);
+    if (search == array[mid]){
+      return mid;
+    } else if(search > array[mid]){
+      lo = mid+1;
+    } else {
+      hi = mid-1;
+    }
+  }
+  return -1;
 }
 
 // Driver code
