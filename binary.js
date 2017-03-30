ca787cc2a993e08920d767c8f429540d3b35ff8a
Parel Wellman Hutahaean <parel.hutahaean@gmail.com>
2017-03-31 00:01:07 +1400

    Binary Search Finished

--- binary.js
+++ binary.js
@@ -28,7 +28,22 @@ var test_array_ganjil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 function binary_search (search, array) {
   // Your code here
-
+  let mid = 0;
+  let left = 0;
+  let right = array.length - 1;
+  let idx = -1;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (array[mid] == search) {
+      idx = mid;
+      break;
+    } else if (search < array[mid]) {
+      right = mid - 1;
+    } else if (search > array[mid]) {
+      left = mid + 1;
+    }
+  }
+  return idx;
 }
 
 // Driver code
